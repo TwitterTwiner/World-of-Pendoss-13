cf8a5e1c8bb40df84da9f726a108d0056a3d6526
FeudeyTF <yanmordanenko089@gmail.com>
2025-08-13 15:11:55 +0300

    Added new rune UI

--- tgui/packages/tgui/index.tsx
+++ tgui/packages/tgui/index.tsx
@@ -25,6 +25,7 @@ import './styles/themes/retro.scss';
 import './styles/themes/syndicate.scss';
 import './styles/themes/wizard.scss';
 import './styles/themes/admin.scss';
+import './styles/themes/baali.scss';
 
 import { perf } from 'common/perf';
 import { setupGlobalEvents } from 'tgui-core/events';
