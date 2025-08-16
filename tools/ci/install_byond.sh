f8eedc2e5fc4b0ede16e63b8f9e9e5203864c42f
FeudeyTF <yanmordanenko089@gmail.com>
2025-08-16 16:39:16 +0300

    r

--- tools/ci/install_byond.sh
+++ tools/ci/install_byond.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-# BYOND_MAJOR and BYOND_MINOR can be explicitly set, such as in alt_byond_versions.txt
-if [ -z "${BYOND_MAJOR+x}" ]; then
-  source dependencies.sh
-fi
+source dependencies.sh
 
 if [ -d "$HOME/BYOND/byond/bin" ] && grep -Fxq "${BYOND_MAJOR}.${BYOND_MINOR}" $HOME/BYOND/version.txt;
 then
@@ -14,7 +11,7 @@ else
   rm -rf "$HOME/BYOND"
   mkdir -p "$HOME/BYOND"
   cd "$HOME/BYOND"
-  curl "https://thefinalnights.com/516.1663_byond_linux.zip" -o byond.zip -A "The-Final-Nights/2.0 Continuous Integration"
+  curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
   unzip byond.zip
   rm byond.zip
   cd byond
