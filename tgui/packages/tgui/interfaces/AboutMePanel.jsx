fdee6c8dab7e474b811bfc880906752b696fe97b
FeudeyTF <yanmordanenko089@gmail.com>
2025-08-29 02:28:46 +0300

    Форматирование интерфейса

--- tgui/packages/tgui/interfaces/AboutMePanel.jsx
+++ tgui/packages/tgui/interfaces/AboutMePanel.jsx
@@ -9,32 +9,37 @@ export const AboutMePanel = (props) => {
     <Window title="About me" width={400} height={500}>
       <Window.Content scrollable>
         <Section>
-        I am {data.name}, the {data.affiliation}. Carrying the {data.role}  {data.special_role && <span style={{ color: "red" }}>{data.special_role}</span>} role
+          I am {data.name}, the {data.affiliation}. Carrying the {data.role}{' '}
+          {data.special_role && (
+            <span style={{ color: 'red' }}>{data.special_role}</span>
+          )}{' '}
+          role
         </Section>
-        {data.info.map((i, infoIndex) =>
-          i.values.len !== 0 && (
-            <Section
-              title={i.name}
-              key={`${i.name}_section_${infoIndex}`}
-              buttons={
-                <Button
-                  color="transparent"
-                  tooltip={i.tooltip}
-                  tooltipPosition="bottom-start"
-                  icon="info"
-                />
-              }>
-              <Stack vertical>
-                {i.values.map((value, index) => (
-                  <Stack.Item key={`${i.name}_${index}`}>
-                    <Section>
-                      {value}
-                    </Section>
-                  </Stack.Item>
-                ))}
-              </Stack>
-            </Section>
-        ))}
+        {data.info.map(
+          (i, infoIndex) =>
+            i.values.len !== 0 && (
+              <Section
+                title={i.name}
+                key={`${i.name}_section_${infoIndex}`}
+                buttons={
+                  <Button
+                    color="transparent"
+                    tooltip={i.tooltip}
+                    tooltipPosition="bottom-start"
+                    icon="info"
+                  />
+                }
+              >
+                <Stack vertical>
+                  {i.values.map((value, index) => (
+                    <Stack.Item key={`${i.name}_${index}`}>
+                      <Section>{value}</Section>
+                    </Stack.Item>
+                  ))}
+                </Stack>
+              </Section>
+            ),
+        )}
       </Window.Content>
     </Window>
   );
